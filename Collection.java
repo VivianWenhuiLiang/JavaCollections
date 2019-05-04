285a0d93b65beedd52cbaee58b0d5f982539c1e6
VivianWenhuiLiang <Vivian.Wenhui.Liang@gmail.com>
2019-05-04 16:48:52 -0700

    update collection
    collection source code

---
+++ Collection.java
@@ -0,0 +1,17 @@
+public Interface Collection<E>{
+    public boolean  add(E e);
+    
+    public boolean contains(Object o);
+    
+    public boolean equals(Object o);
+    
+    public isEmpty();
+    
+    public Iterator<E> iterator();
+    
+    public boolean remove(Object o);
+    
+    public Int size();
+    
+    public Object[] toArray();
+}    
