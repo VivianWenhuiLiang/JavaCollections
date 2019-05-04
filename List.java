d5788e1b1e56e6ed553d643753a3254975b434cf
VivianWenhuiLiang <Vivian.Wenhui.Liang@gmail.com>
2019-05-04 16:35:07 -0700

    update List
    update source code

---
+++ List.java
@@ -0,0 +1,19 @@
+public Interface List<E>{
+    public  void      add(int index, E element);
+    
+    public  boolean   contains(Object  o);
+    
+    public  E        get(int index);
+    
+    public  boolean  isEmpty();
+    
+    public  Iterator<E>  iterator();
+    
+    public  E           remove(Object o);
+    
+    public  E           set(int index, E element);
+    
+    public  int      size();
+}    
+    
+  
