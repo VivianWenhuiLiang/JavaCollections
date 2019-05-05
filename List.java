6d3bd989008e089b1338bd7ea8939944764fed21
VivianWenhuiLiang <Vivian.Wenhui.Liang@gmail.com>
2019-05-05 14:30:25 -0700

    New branch (#2)
    * add all the collection files
    
    * add source code
    
    add source code
    
    * update List
    
    update source code
    
    * update collection
    
    collection source code
    
    * update Queue
    
    update interface queue source code
    
    * update set
    
    update interface Set source code
    
    * update deque
    
    deque source code
    
    * update SortedSet
    
    SortedSet source code
    
    * Add Iterator and fix bugs in Collections
    
    * add the source for all the interfaces

---
+++ List.java
@@ -0,0 +1,8 @@
+public interface List<E> extends Collection<E> {
+    public void add(int index, E element);
+    public E get(int index);
+    public E remove(int index);
+    public E set(int index, E element);
+}    
+    
+  
