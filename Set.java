5ff83c06d27a06d5ad589109df24625370e8d306
VivianWenhuiLiang <Vivian.Wenhui.Liang@gmail.com>
2019-05-04 17:04:15 -0700

    update set
    update interface Set source code

---
+++ Set.java
@@ -0,0 +1,11 @@
+public Interface Set<E>{
+   public boolean add(E e);
+   public void clear();
+   public boolean contains(Object o);
+   public boolean isEmpty();
+   public Iterator<E> iterator();
+   public boolean remove(Object o);
+   public  int size();
+   public  Object[] toArray();
+}
+   
